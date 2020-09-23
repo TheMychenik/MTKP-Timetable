e1f8bb9e9236526654b10ce976d3809704b492a8
Alexey Rakhmaninov <46868290+themychenik@users.noreply.github.com>
2020-09-23 18:39:39 +0300

    weeks check fix

--- BotBrain/parsing/scheduleparser.py
+++ BotBrain/parsing/scheduleparser.py
@@ -61,7 +61,7 @@ def __get_lessons(sheet):
                             else:
                                 # [group_name, day, isupper, islef, [(less_1, cab_1), ..., (less_6, cab_6)]]
 
-                                isupper = weekday.check_isweek_upper(day)  # верхняя/нижняя неделя
+                                isupper = False if lesson_name.row % 2 == 0 else True  # верхняя/нижняя неделя
                                 if lesson_name.value is not None:
                                     lesson = clear_invisible_character(lesson_name.value, separator=' ')
                                     cab = __get_cab(sheet, lesson_name.row, cabinet_col)
