fc5b931f571a544e77209c6277543b3282a4fde8
Alexey Rakhmaninov <46868290+themychenik@users.noreply.github.com>
2020-09-28 01:02:19 +0300

    help output

--- BotBrain/commands/help.py
+++ BotBrain/commands/help.py
@@ -2,10 +2,9 @@ from .. import command_system
 
 
 def help_(*_):
-    message = '---------\n'
+    message = ''
     for command in command_system.command_list:
-        message += f'[{command.keys[0].capitalize()}] - {command.description}\n'
-        message += '---------\n'
+        message += f'[{command.keys[0].capitalize()}] - {command.description}\n\n'
     return message, ('text',)
 
 
